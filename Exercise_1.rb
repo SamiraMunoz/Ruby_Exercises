90ccdc294c8e0cef57accadc30ce8b0a3d03e6fc
Samira Munoz <samira.munoz@koombea.com>
2019-07-22 17:53:34 -0500

    Multiples of 3 and 5 v3

--- Exercise_1.rb
+++ Exercise_1.rb
@@ -1,8 +1,8 @@
-class Multiples_3_and_5
+class Multiples3and5
   def sum_multiples
     sum  = (1...1000).select { |num| (num % 3) == 0 || (num % 5) == 0 }.sum
   end
 end
-sum_below_1000 = Multiples_3_and_5.new
+sum_below_1000 = Multiples3and5.new
 puts "The sum of all multiples of 3 or 5 below 1000 is: #{sum_below_1000.sum_multiples}"
 
