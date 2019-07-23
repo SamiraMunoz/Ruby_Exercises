28173a345a0599a38f7da7a8eceafa5fbdeec968
Samira Munoz <samira.munoz@koombea.com>
2019-07-23 15:27:21 -0500

    Palindrome v4

--- Exercise_6_Reverse.rb
+++ Exercise_6_Reverse.rb
@@ -2,11 +2,12 @@ class Palindrome
   def initialize(word)
     @word = word
   end
-  def palindrome_word
-  	@word == @word.reverse ? true : false
+  def equal_letters?
+  	@word == @word.reverse 
   end
 end
 puts "Enter a word"
 word = (gets.chomp).downcase
 is_palindrome = Palindrome.new(word)
-puts is_palindrome.palindrome_word == true ? "Is palindrome" : "It's not palindrome"
+puts is_palindrome.equal_letters? == true ? "Is palindrome" : "It's not palindrome" 
+
