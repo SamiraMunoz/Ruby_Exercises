45458604f6037a1b83d2391f8b7659dc2e71ec9f
Samira Munoz <samira.munoz@koombea.com>
2019-07-23 09:37:06 -0500

    Sum square difference v3

--- Exercise_8.rb
+++ Exercise_8.rb
@@ -1,10 +1,10 @@
-class Difference
-  def sum_square()
+class DifferenceSquare
+  def sum_square
     sum_square = (1..100).inject { |sum, num| sum + num**2 }
     sum_num = ((1..100).inject { |sum, num| sum + num })**2
-    subtraction = ( sum_num -  sum_square)
-    puts "The difference is #{subtraction}"
+    sum_num -  sum_square
   end
 end
-exercise = Difference.new
-exercise.sum_square()
+difference = DifferenceSquare.new
+difference.sum_square
+puts "The difference is #{difference.sum_square}"
