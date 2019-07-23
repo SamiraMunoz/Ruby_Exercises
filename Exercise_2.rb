4870822f8c2566e6db18dba10edac86439d88b55
Samira Munoz <samira.munoz@koombea.com>
2019-07-23 14:33:23 -0500

    Fibonacci v4

--- Exercise_2.rb
+++ Exercise_2.rb
@@ -1,12 +1,12 @@
 class Fibonacci
   def sum_even
     fibonacci = []
-    a,b,c = 1,1,1
+    a,b,c = 0,1,0
     loop do
-      fibonacci << c if c.even? 
       c = a + b
       a = b
       b = c
+      fibonacci << c if c.even? 
       break if c > 4_000_000
     end
     fibonacci.inject { |sum, n| sum + n } 
