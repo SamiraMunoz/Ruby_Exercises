5af06574731bc4cbffe30efa5542befba3d2966e
Samira Munoz <samira.munoz@koombea.com>
2019-07-23 11:54:14 -0500

    remove Exercise_1.rb

--- Exercise_1.rb
+++
@@ -1,7 +0,0 @@
-class Multiples3And5
-  def sum_multiples
-    sum  = (1...1000).select { |num| (num % 3) == 0 || (num % 5) == 0 }.sum
-  end
-end
-exercise = Multiples3And5.new
-puts "The sum of all multiples of 3 or 5 below 1000 is: #{exercise.sum_multiples}."
