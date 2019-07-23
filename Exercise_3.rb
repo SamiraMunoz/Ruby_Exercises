2ef9b188396359b9b7057ac4f18e1f1b4c1f8007
Samira Munoz <samira.munoz@koombea.com>
2019-07-23 14:46:41 -0500

    Largest prime factor v4

--- Exercise_3.rb
+++ Exercise_3.rb
@@ -3,15 +3,14 @@ class PrimeFactors
     @largest_factor = largest_factor
   end
   def largest_prime_factor
-    factors = []
+    factor = 0
     2.upto(@largest_factor) do |num|
       next if @largest_factor % num != 0
-      factors << num
+      factor = num
       @largest_factor = @largest_factor/num
     end
-    factors.each { |num| num }
+    factor
   end
 end
-puts "The factors of 13195 are: "
 factor = PrimeFactors.new(13195)
-puts factor.largest_prime_factor
+puts "The largest prime factor is: #{factor.largest_prime_factor}."
