e7da7298b4ef175b46af4cb466980b02030c7a02
Samira Munoz <samira.munoz@koombea.com>
2019-07-24 15:08:10 -0500

    Multiples 3 and 5	Fibonacci v4

---
+++ Test/Exercise_2_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+
+def sum_even
+  fibonacci = []
+  a,b,c = 0,1,0
+  loop do
+    c = a + b
+    a = b
+    b = c
+    fibonacci << c if c.even? 
+    break if c > 4_000_000
+  end
+  fibonacci.inject { |sum, n| sum + n } 
+end
+class DifferenceSquare < Minitest::Test
+  def test_multiples
+    assert_equal 4613732, sum_even
+  end
+end
