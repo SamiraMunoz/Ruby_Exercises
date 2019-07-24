ab64ba298127bf71cc59f5dd50e93430cd54b156
Samira Munoz <samira.munoz@koombea.com>
2019-07-24 16:04:28 -0500

    Largest palindrome product test

--- Test/Exercise_4_test.rb
+++ Test/Exercise_4_test.rb
@@ -1,19 +1,9 @@
 require 'minitest/autorun'
+require '../Exercise:4.rb'
 
-def three_digit_product
-  mayor = 0
-  700.upto(999) do |x|
-    x.upto(999) do |y|
-      num = (x * y).to_s
-      next if num != num.reverse
-      num = num.to_i
-      mayor = num if mayor < num
-    end
-  end
-  mayor
-end
-class Palindrome < Minitest::Test
+class Palindrom < Minitest::Test
   def test_multiples
-    assert_equal 906609, three_digit_product
+    palindrome = Palindrome.new
+    assert_equal 906609, palindrome.three_digit_product
   end
 end
