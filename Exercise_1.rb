b7fc34c7c3a66b103fba91583560c4937ba2689f
Samira Munoz <samira.munoz@koombea.com>
2019-07-24 14:04:31 -0500

    Multiples of 3 and 5 v5

--- Exercise_1.rb
+++ Exercise_1.rb
@@ -1,6 +1,7 @@
 class Multiples3and5
   def sum_multiples
-    (1...1000).select { |num| (num % 3) == 0 || (num % 5) == 0 }.sum
+    mul_3_and_5 = (1...1000).select { |num| (num % 3) == 0 || (num % 5) == 0 }
+    mul_3_and_5.inject { |sum, num| sum + num }
   end
 end
 sum_below_1000 = Multiples3and5.new
