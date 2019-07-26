e43e9393058610452d79263d9a0f5c2f00d12fe4
Samira Munoz <samira.munoz@koombea.com>
2019-07-26 11:50:33 -0500

    Largest palimdrome product test

--- Exercise_4_Parametros.rb
+++ Exercise_4_Parametros.rb
@@ -1,20 +1,38 @@
 class Palindrome
-  def initialize(digits)
-    @digits = digits
+  def initialize(digit)
+    self.parse_digits(digit)
   end
-  def digit_product
+  def parse_digits(digit)
     begin
-      raise ArgumentError, "The argument is not an integer greater than 1" unless @digits > 1
-      start_digits = ("#{7}#{"0"*(@digits-1)}").to_i
-      end_digits = ("#{9}#{"9"*(@digits-1)}").to_i
-      mayor = 0
-      start_digits.upto(end_digits) do |x|
-        x.upto(end_digits) do |y|
-          num = (x * y).to_s
-          next if num != num.reverse
-          num = num.to_i
-          mayor = num if mayor < num
-        end
+      @digits = digit.to_i 
+    rescue digit.class == String
+      msg = "The argunment is a String"
+      raise ArgumentError, msg, caller 
+      return msg
+    rescue digit < 1
+      msg = "The argument is not an integer greater than 1"
+      raise ArgumentError, msg , caller 
+      return msg
+    rescue digit.nil?
+      msg = "The argument is empty"
+      raise ArgumentError, msg , caller
+      return msg
+    end
+  end
+  def start_digits
+    ("#{7}#{"0"*(@digits-1)}").to_i
+  end
+  def end_digits
+    ("#{9}#{"9"*(@digits-1)}").to_i
+  end  
+  def digit_product     
+    mayor = 0
+    start_digits.upto(end_digits) do |x|
+    x.upto(end_digits) do |y|
+      num = (x * y).to_s
+        next if num != num.reverse
+        num = num.to_i
+        mayor = num if mayor < num
       end
     end
     mayor
--- Test/Exercise_4_Parametros_test.rb
+++ Test/Exercise_4_Parametros_test.rb
@@ -3,36 +3,34 @@ require '../Exercise_4_Parametros.rb'
 
 class Palindrom < Minitest::Test
   # when the parameter is 3 I should expect the response to be 906609
-  def test_multiples
+  def test_palindrome
     palindrome = Palindrome.new(3)
     assert_equal 906609, palindrome.digit_product
   end
-  # when the parameter is string I should expect the response to be "The argument is not an integer greater than 1"
-  def test_multiples_parameter_string
+  # when the parameter is "2" I should expect the response to be 9009
+  def test_palindrome_parameter_string_number
+    palindrome = Palindrome.new("2")
+    assert_equal 9009, palindrome.digit_product
+  end
+  # when the parameter is "Hola mundo" I should expect the response to be "The argunment is a Strings"
+  def test_palindrome_parameter_string
     palindrome = Palindrome.new("Hola mundo")
     assert_raises ArgumentError do 
-      palindrome.digit_product
+      assert_equal "The argunment is a String", palindrome.digit_product
     end
   end
-  # when the parameter is string I should expect the response to be "The argument is not an integer greater than 1"
-  def test_multiples_parameter_0
+  # when the parameter is 0 I should expect the response to be "The argument is not an integer greater than 1"
+  def test_palindrome_parameter_0
     palindrome = Palindrome.new(0)
     assert_raises ArgumentError do 
-      palindrome.digit_product
+      assert_equal "The argument is not an integer greater than 1", palindrome.digit_product
     end
   end
-  # when the parameter is string I should expect the response to be "The argument is not an integer greater than 1"
-  def test_multiples_parameter_negative
-    palindrome = Palindrome.new(-1)
+  # when the parameter is '' I should expect the response to be "The argument is empty"
+  def test_palindrome_parameter_empty
+    palindrome = Palindrome.new('')
     assert_raises ArgumentError do 
-      palindrome.digit_product
-    end
-  end
-  # when the parameter is string I should expect the response to be "The argument is not an integer greater than 1"
-  def test_multiples_parameter_1
-    palindrome = Palindrome.new(1)
-    assert_raises ArgumentError do 
-      palindrome.digit_product
+      assert_equal "The argument is empty",palindrome.digit_product
     end
   end
 end
