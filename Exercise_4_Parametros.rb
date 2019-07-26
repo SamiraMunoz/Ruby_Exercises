62075cfa0f41fb23e0832100b318c49011de6c66
Samira Munoz <samira.munoz@koombea.com>
2019-07-26 17:15:11 -0500

    Largest palindrome product test

--- Exercise_4_Parametros.rb
+++ Exercise_4_Parametros.rb
@@ -1,31 +1,48 @@
+class EmptyDigitError < StandardError; end
+class InvalidStringError < StandardError; end
+class InvalidIntegerError < StandardError; end
+
 class Palindrome
   def initialize(digit)
-    self.parse_digits(digit)
+    @digits = self.parse_digits(digit)
   end
+
   def parse_digits(digit)
-    begin
-      @digits = digit.to_i 
-    rescue digit.class == String
-      mesg = "The argunment is a String"
-      raise ArgumentError, mesg, caller 
-      return mesg
-    rescue digit < 1
-      mesg = "The argument is not an integer greater than 1"
-      raise ArgumentError, mesg , caller 
-      return mesg
-    rescue digit.nil?
-      mesg = "The argument is empty"
-      raise ArgumentError, mesg , caller
-      return mesg
+    return if invalid_digit?(digit)
+    digit.to_i
+  end
+
+  def invalid_digit?(digit)
+    if digit_empty?(digit)
+      raise EmptyDigitError, 'The argument is empty'
+    elsif digit_string?(digit)
+      raise InvalidStringError, 'The argunment is a String'
+    elsif digit_less_than_1?(digit)
+      raise InvalidIntegerError, 'The argument is not an integer greater than 1'
     end
   end
-  def start_digits
-    ("#{7}#{"0"*(@digits-1)}").to_i
+
+  def digit_empty?(digit)
+    digit.nil? || digit == ''
   end
+
+  def digit_string?(digit)
+    digit.class == String && digit.to_i == 0
+  end
+
+  def digit_less_than_1?(digit)
+    digit.to_i < 1
+  end
+
+  def start_digits
+    ("#{7}#{'0'*(@digits-1)}").to_i
+  end
+
   def end_digits
-    ("#{9}#{"9"*(@digits-1)}").to_i
-  end  
-  def digit_product     
+    ("#{9}#{'9'*(@digits-1)}").to_i
+  end
+
+  def digit_product
     mayor = 0
     start_digits.upto(end_digits) do |x|
     x.upto(end_digits) do |y|
--- Test/Exercise_4_Parametros_test.rb
+++ Test/Exercise_4_Parametros_test.rb
@@ -7,30 +7,30 @@ class Palindrom < Minitest::Test
     palindrome = Palindrome.new(3)
     assert_equal 906609, palindrome.digit_product
   end
-  # when the parameter is "2" I should expect the response to be 9009
+  # when the parameter is '2' I should expect the response to be 9009
   def test_palindrome_parameter_string_number
-    palindrome = Palindrome.new("2")
+    palindrome = Palindrome.new('2')
     assert_equal 9009, palindrome.digit_product
   end
-  # when the parameter is "Hola mundo" I should expect the response to be "The argunment is a Strings"
+  # when the parameter is 'Hola mundo' I should expect the response to be 'The argunment is a Strings'
   def test_palindrome_parameter_string
-    palindrome = Palindrome.new("Hola mundo")
-    assert_raises ArgumentError do 
-      assert_equal "The argunment is a String", palindrome.digit_product
+    err = assert_raises InvalidStringError do
+      Palindrome.new('Hola mundo')
     end
+    assert_match 'The argunment is a String', err.message
   end
-  # when the parameter is 0 I should expect the response to be "The argument is not an integer greater than 1"
+  # when the parameter is 0 I should expect the response to be 'The argument is not an integer greater than 1'
   def test_palindrome_parameter_0
-    palindrome = Palindrome.new(0)
-    assert_raises ArgumentError do 
-      assert_equal "The argument is not an integer greater than 1", palindrome.digit_product
+    err = assert_raises InvalidIntegerError do
+      Palindrome.new(0)
     end
+    assert_match 'The argument is not an integer greater than 1', err.message
   end
-  # when the parameter is '' I should expect the response to be "The argument is empty"
+  # when the parameter is '' I should expect the response to be 'The argument is empty'
   def test_palindrome_parameter_empty
-    palindrome = Palindrome.new('')
-    assert_raises ArgumentError do 
-      assert_equal "The argument is empty",palindrome.digit_product
+    err = assert_raises EmptyDigitError do
+      Palindrome.new('')
     end
+    assert_match 'The argument is empty', err.message
   end
 end
