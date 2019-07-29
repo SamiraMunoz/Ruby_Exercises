4b09ffad073cf0e68beba8aad8395bb7ada2939e
Samira Munoz <samira.munoz@koombea.com>
2019-07-29 09:41:53 -0500

    10001st Prime test

--- Exercise_5.rb
+++ Exercise_5.rb
@@ -1,9 +1,50 @@
 require 'prime'
-class Prime10001
+
+class EmptyNumberError < StandardError; end
+class InvalidStringError < StandardError; end
+class NumberNegativeError < StandardError; end
+class InvalidFloatError < StandardError; end
+
+class PrimeNumber
+  def initialize(number)
+    @number = self.parse_number(number)
+  end
+
+  def parse_number(number)
+    return if invalid_number?(number)
+    number.to_i
+  end
+
+  def invalid_number?(number)
+    if number_empty?(number)
+      raise EmptyNumberError, 'The argument is empty'
+    elsif number_string?(number)
+      raise InvalidStringError, 'The argunment is a String'
+    elsif number_negative_or_less_than_2?(number)
+      raise NumberNegativeError, 'The argument is a integer negative or less than 2'
+    elsif number_float?(number)
+      raise InvalidFloatError, 'The argument is float'
+    end
+  end
+
+  def number_empty?(number)
+    number.nil? || number == ''
+  end
+
+  def number_string?(number)
+    number.class == String && number.to_i == 0
+  end
+
+  def number_negative_or_less_than_2?(number)
+    number.to_i < 2
+  end
+
+  def number_float?(number)
+    number.class == Float
+  end
+
   def primes
-    prime = Prime.first 10001
+    prime = Prime.first @number
     prime.max
   end
 end
-prime = Prime10001.new
-puts "The 10 001st prime number is: #{prime.primes}"
--- Test/Exercise_5_test.rb
+++ Test/Exercise_5_test.rb
@@ -1,9 +1,56 @@
 require 'minitest/autorun'
 require '../Exercise_5.rb'
 
-class Palindrom < Minitest::Test
-  def test_multiples
-    prime = Prime10001.new
-    assert_equal 104743, prime.primes
+class Prim < Minitest::Test
+  # when the parameter is 10 I should expect the response to be 29
+  def test_prime
+    prime = PrimeNumber.new(10)
+    assert_equal 29, prime.primes
+  end
+
+  # when the parameter is '100' I should expect the response to be 541
+  def test_prime_parameter_string_number
+    prime = PrimeNumber.new('100')
+    assert_equal 541, prime.primes
+  end
+
+  # when the parameter is -4 I should expect the response to be 'The argument is a integer negative or less than 2'
+  def test_prime_parameter_negative_number
+    err = assert_raises NumberNegativeError do
+      PrimeNumber.new(-4)
+    end
+    assert_match 'The argument is a integer negative or less than 2', err.message
+  end
+
+  # when the parameter is 'Hola mundo' I should expect the response to be 'The argunment is a Strings'
+  def test_prime_parameter_string
+    err = assert_raises InvalidStringError do
+      PrimeNumber.new('Hola mundo')
+    end
+    assert_match 'The argunment is a String', err.message
+  end
+
+  # when the parameter is '' I should expect the response to be 'The argument is empty'
+  def test_prime_parameter_empty
+    err = assert_raises EmptyNumberError do
+      PrimeNumber.new('')
+    end
+    assert_match 'The argument is empty', err.message
+  end
+
+  # when the parameter is 7.4 I should expect the response to be 'The argument is float'
+  def test_prime_parameter_float
+    err = assert_raises InvalidFloatError do
+      PrimeNumber.new(7.4)
+    end
+    assert_match 'The argument is float', err.message
+  end  
+
+# when the parameter is 0 I should expect the response to be 'The argument is a integer negative or less than 2'
+  def test_prime_parameter_negative_number
+    err = assert_raises NumberNegativeError do
+      PrimeNumber.new(0)
+    end
+    assert_match 'The argument is a integer negative or less than 2', err.message
   end
 end
