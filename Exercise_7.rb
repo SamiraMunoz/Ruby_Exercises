e0314923eb8220d527f0bbce883dce764beb0f92
Samira Munoz <samira.munoz@koombea.com>
2019-07-29 10:14:37 -0500

    Factorial

--- Exercise_7.rb
+++ Exercise_7.rb
@@ -1,12 +1,40 @@
+class EmptyNumberError < StandardError; end
+class InvalidStringError < StandardError; end
+class InvalidFloatError < StandardError; end
+
 class FactorialNumber
-  def initialize(num)
-    @num = num
+  def initialize(number)
+    @number = self.parse_number(number)
   end
+
+  def parse_number(number)
+    return if invalid_number?(number)
+    number.to_i.abs
+  end
+
+  def invalid_number?(number)
+    if number_empty?(number)
+      raise EmptyNumberError, 'The argument is empty'
+    elsif number_string?(number)
+      raise InvalidStringError, 'The argunment is a String'
+    elsif number_float?(number)
+      raise InvalidFloatError, 'The argument is float'
+    end
+  end
+
+  def number_empty?(number)
+    number.nil? || number == ''
+  end
+
+  def number_string?(number)
+    number.class == String && number.to_i == 0
+  end
+
+  def number_float?(number)
+    number.class == Float
+  end
+
   def factorial
-    (1..@num).inject(1) { |product, num| product * num }
+    (1..@number).inject(1) { |product, num| product * num }
   end
 end
-puts "Enter a number to give your factorial."
-num = Integer(gets.chomp).abs
-factorial_num = FactorialNumber.new(num)
-puts "The factorial number is: #{factorial_num.factorial}"
---
+++ Test/Exercise_7_test.rb
@@ -0,0 +1,52 @@
+require 'minitest/autorun'
+require '../Exercise_6_Samira.rb'
+
+class Factoria < Minitest::Test
+  # when the parameter is 4 I should expect the response to be 24
+  def test_factorial
+    factorial_num = FactorialNumber.new(4)
+    assert_equal 24, factorial_num.factorial
+  end
+
+  # when the parameter is '4' I should expect the response to be 24
+  def test_factorial_parameter_string_number
+    factorial_num = FactorialNumber.new('4')
+    assert_equal 24, factorial_num.factorial
+  end
+
+   # when the parameter is -4 I should expect the response to be 24
+  def test_factorial_parameter_negative_number
+    factorial_num = FactorialNumber.new(-4)
+    assert_equal 24, factorial_num.factorial
+  end
+
+  # when the parameter is -4 I should expect the response to be 24
+  def test_factorial_parameter_0
+    factorial_num = FactorialNumber.new(0)
+    assert_equal 1, factorial_num.factorial
+  end
+
+  # when the parameter is 'Hola mundo' I should expect the response to be 'The argunment is a Strings'
+  def test_factorial_parameter_string
+    err = assert_raises InvalidStringError do
+      FactorialNumber.new('Hola mundo')
+    end
+    assert_match 'The argunment is a String', err.message
+  end
+
+  # when the parameter is '' I should expect the response to be 'The argument is empty'
+  def test_factorial_parameter_empty
+    err = assert_raises EmptyNumberError do
+      FactorialNumber.new(nil)
+    end
+    assert_match 'The argument is empty', err.message
+  end
+
+  # when the parameter is 7.4 I should expect the response to be 'The argument is float'
+  def test_factorial_parameter_float
+    err = assert_raises InvalidFloatError do
+      FactorialNumber.new(7.4)
+    end
+    assert_match 'The argument is float', err.message
+  end  
+end
